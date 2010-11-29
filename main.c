431820dab7420545129acdcb9ac1f60cf699e185
joeyrobert <joey@joeyrobert.org>
2010-11-29 11:31:18 -0800

    useful debuggin'

--- main.c
+++ main.c
@@ -6,10 +6,13 @@
 
 int main() {
     unsigned long long result;
+    unsigned count, moves[256];
     board_set_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    count = gen_moves(moves);
+    board_add(moves[6]);
+    count = gen_moves(moves);
+    board_add(moves[0]);
     board_draw();
-    result = engine_perft(3); 
-    board_draw();
-    
+    engine_divide(1);
     return 0;
 }
