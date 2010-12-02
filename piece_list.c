511364af46dbf4070821f537a2d502ea4d772499
joeyrobert <joey@joeyrobert.org>
2010-12-01 20:24:44 -0800

    delete unnecessary bounds checks.

--- piece_list.c
+++ piece_list.c
@@ -16,21 +16,17 @@ void piece_list_new(piece_list* list) {
 }
 
 void piece_list_add(piece_list* list, unsigned index) {
-    if(list->count < 16) {
-        list->index[list->count] = index;
-        list->reverse[index] = list->count;
-        list->count += 1;
-    }
+    list->index[list->count] = index;
+    list->reverse[index] = list->count;
+    list->count += 1;
 }
 
 void piece_list_subtract(piece_list* list, unsigned index) {
     unsigned loc;
-    if(list->count > 0) {
-        loc = list->reverse[index];
-        list->count -= 1;
-        list->index[loc] = list->index[list->count];
-        list->reverse[list->index[loc]] = loc;
-        list->index[list->count] = EMPTY;
-        list->reverse[index] = EMPTY;
-    }
+    loc = list->reverse[index];
+    list->count -= 1;
+    list->index[loc] = list->index[list->count];
+    list->reverse[list->index[loc]] = loc;
+    list->index[list->count] = EMPTY;
+    list->reverse[index] = EMPTY;
 }
